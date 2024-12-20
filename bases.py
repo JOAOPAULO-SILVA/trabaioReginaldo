aa62f3cb5b502f7b77cc595bb7fdc23807e1d2df
jP <silva.joao@icen.ufpa.br>
2024-12-20 20:59:03 -0300

    Solução do desafio 09.

--- bases.py
+++ bases.py
@@ -1,5 +1,23 @@
-base_entrada, base_saida, numero_entrada = map(str,input("Digite a base de entrada a base de saida e o numer à ser convertido: ").split(" "))
-base_saida = int(base_saida)
+entradas = [
+[2 ,3 ,0],
+[11 ,11 ,99],
+[10, 2 ,100],
+[10, 16, -99],
+[17, 10, "2018ABDG"],
+[36 ,16 ,"OHYEAHTHISISBIG"],
+[62, 16, "OHYEAHTHISISBIG"],
+[35, 2, "OHYEZZZZ"],
+[0, 2, 100],
+[2 ,0, 100],
+[62, 2, "zooooooooooooooooooooooooooom"],
+[62, 16,"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz" ],
+[35, 3, "TERNARIO"],
+[62, 16, "1zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"],
+[33, 8, "OCTAL"],
+[10, 62, "27805568394691458661136458099556259144705"],
+[16, 62, "128F36A9A4BD3E9D666B6340C7EA3"],
+[5, 62, 10212030103214130013104]
+]
 dicionario = {
     "0" : 0,
     "1" : 1,
@@ -86,9 +104,29 @@ def base10tobaseX(numBase_10, base_saida):
         resto = dividendo % base_saida
         numeroFinal = ValueInDict(resto) + numeroFinal
         dividendo = dividendo // base_saida
-    numeroFinal = str(dividendo) + numeroFinal
+    numeroFinal = ValueInDict(dividendo) + numeroFinal
     return numeroFinal
 
-numero_com_base10 = converttobase10(base_entrada,numero_entrada)
-resultado = base10tobaseX(numero_com_base10, base_saida)
-print(resultado)
+def verificar(baseEntrada, baseSaida, numeroEntrada):
+    for n in numeroEntrada:
+        if n == "-":
+            print("???")
+            return False
+        elif dicionario[n] > (int(baseEntrada)-1):
+            print("???")
+            return False
+    if int(baseEntrada)<2 or int(baseEntrada)>62 or baseSaida<2 or baseSaida>62:
+        print("???")
+        return False
+    if converttobase10(baseEntrada,numeroEntrada) > 591222134364399413463902591994678504204696392694759423:
+        print("???")
+        return False
+    return True
+
+for entrada in entradas:
+    base_entrada, base_saida, numero_entrada = map(str,entrada)
+    base_saida = int(base_saida)
+    if verificar(base_entrada,base_saida,numero_entrada):
+        numero_com_base10 = converttobase10(base_entrada,numero_entrada)
+        resultado = base10tobaseX(numero_com_base10, base_saida)
+        print(resultado)
---
+++ teste.py
@@ -0,0 +1,9 @@
+import bases as bs
+# for entrada in entradas:
+#     base_entrada, base_saida, numero_entrada = map(str,entrada)
+#     base_saida = int(base_saida)
+#     if verificar():
+#         numero_com_base10 = converttobase10(base_entrada,numero_entrada)
+#         resultado = base10tobaseX(numero_com_base10, base_saida)
+#         print(resultado)
+# print(bs.converttobase10(62,"zzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"))
